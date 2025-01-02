98e2ada6a56114a87ca0156d15992634881cf546
Rolf Beab <rolf@lane4.digital>
2025-01-02 11:56:08 +0100

    avoid commits to main now using more professional git workflow

--- support/pre-commit-hook.sh
+++ support/pre-commit-hook.sh
@@ -10,19 +10,19 @@ files=$(git diff --cached --name-only --diff-filter=ACMR -- '*.php')
 
 branch="$(git rev-parse --abbrev-ref HEAD)"
 commits="$(git rev-list --count HEAD ^${branch})"
-pattern="^(feature|fix|hotfix)\/[0-9]{6,7}_[a-zA-Z0-9_-]+|:[0-9a-f]{7,40}$"
+pattern="^(feature|fix|hotfix)\/[0-9]{1,7}_[a-zA-Z0-9_-]+|:[0-9a-f]{7,40}$"
 
 if [[ -d "${gitDir}/rebase-merge" || -d "${gitDir}/rebase-apply" ]]; then
     exit 0;
 fi
 
-#if [[ $commits -eq 0 ]]; then
-#    echo "Validate branch name..."
-#    if [[ ! $branch_name =~ $pattern ]]; then
-#        echo -e "\e[1;31mCommit not valid because of branch name '${branch}' !\n\e[0m"
-#        exit 1;
-#    fi
-#fi
+if [[ $commits -eq 0 ]]; then
+    echo "Validate branch name..."
+    if [[ ! $branch =~ $pattern ]]; then
+        echo -e "\e[1;31mCommit not valid because of branch name '${branch}' !\n\e[0m"
+        exit 1;
+    fi
+fi
 
 echo "Committing as user ${user}"
 if [[ $user =~ [.,:'!@#$%^&*()_+'] ]]; then
